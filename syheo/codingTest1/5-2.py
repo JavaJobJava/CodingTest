9182dfbca44ca2fab78912ff8f8d65a8cc4d09ae
heoseungyeon <diao312@naver.com>
2021-02-09 08:48:32 +0900

    재귀

--- syheo/codingTest1/5-2.py
+++ syheo/codingTest1/5-2.py
@@ -13,7 +13,7 @@ queue.append(5)
 queue.append(2)
 queue.append(3)
 queue.append(7)
-queue.popleft()
+queue.popleft() #가장 먼저 들어온 원소 제거 
 queue.append(1)
 queue.append(4)
 queue.popleft()
---
+++ syheo/codingTest1/5-3.py
@@ -0,0 +1,9 @@
+#CH5 BFS&DFS
+#예제 5-3
+# 재귀함수
+
+def recursive_function():
+    print('재귀 함수를 호출합니다.')
+    recursive_function()
+
+recursive_function()
