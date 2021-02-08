c8a7ce360f3848dc12d3f1a4d60a0a4b376fa14e
heoseungyeon <diao312@naver.com>
2021-02-08 16:48:47 +0900

    승연 DFS/BFS

---
+++ syheo/codingTest1/5-1.py
@@ -0,0 +1,18 @@
+#CH5 BFS&DFS
+#예제 5-1
+# 스택
+
+stack = []
+
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+
+#출력 
+print(stack)
+print(stack[::-1])
