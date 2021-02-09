e6e477d407299c7739c7bc6920bed6adb166f919
heoseungyeon <diao312@naver.com>
2021-02-09 14:34:44 +0900

    미로 탈출

--- syheo/codingTest1/5-10.py
+++ syheo/codingTest1/5-10.py
@@ -8,13 +8,15 @@
 #생성되는 총 아이스크림의 개수를 구하는 프로그램
 
 
-
+#입력 
 N,M = map(int,input().split())
 
 graph = [[] for _ in range(N)]
+#방문값 초기화 
 visited = [[False]*M for _ in range(N)]
 sum = 0
 
+#그래프 입력
 for i in range(N):
     temp = list(map(int,input()))
     graph[i]=temp
@@ -22,12 +24,14 @@ for i in range(N):
 #dfs 
 def dfs(row,col):
     cnt=0
+    #예외 처리
     if row<0 or col<0 or row>=N or col>=M:
         pass 
+    #뚜껑 or 방문함
     elif graph[row][col]==1 or visited[row][col]:
         pass
+    #방문 가즈아 
     else:
-        #방문 가즈아 
         visited[row][col]=True
         cnt=1
         
@@ -38,7 +42,7 @@ def dfs(row,col):
 
     return cnt
 
-
+#탐색
 for i in range(N):
     for j in range(M):
         sum+=dfs(i,j)
