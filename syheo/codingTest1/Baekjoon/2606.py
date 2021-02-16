1d43089b6b60bcefad4de5fbcac8cce18a62b4f1
heoseungyeon <dia0312@naver.com>
2021-02-16 15:32:51 +0900

    5430

--- syheo/codingTest1/Baekjoon/2606.py
+++ syheo/codingTest1/Baekjoon/2606.py
@@ -10,6 +10,13 @@
 # 두번 째 줄 : 네트워크 연결 쌍 ex ) 1 2
 # 세번 째 줄 : 연결쌍 값들 
 
+#깊이 우선 탐색으로 
+#연결된 노드 정보가 저장된 인접 리스트를 순회 
+#방문을 할때 1을 더함
+
+from sys import stdin
+input = stdin.readline
+
 N = int(input())
 K = int(input())
 
@@ -21,12 +28,14 @@ for i in range(K):
     linked[tmp[0]].append(tmp[1])
     linked[tmp[1]].append(tmp[0])
 
+#하고 안하고 차이는? 
 for i in range(N):
     linked[i].sort()
 
 def dfs(linked,node,visited):
     cnt = 0
     for i in linked[node]:
+        #방문 처리
         if visited[i] is not True:
             visited[i] = True
             cnt += 1
@@ -36,6 +45,7 @@ def dfs(linked,node,visited):
     return cnt
 #1 컴퓨터 방문 처리
 visited[1] = True
+
 print(dfs(linked,1,visited))
 
 
