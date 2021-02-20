068968a591e0dd693e553680043820f4e8782583
heoseungyeon <dia0312@naver.com>
2021-02-20 11:36:00 +0900

    캥거루 두마리2

---
+++ syheo/codingTest1/Baekjoon/11034.py
@@ -0,0 +1,19 @@
+#solved.ac
+#브론즈3
+#그리디
+#캥거루 세마리2
+#11034
+
+while True:
+    try:
+        kang = list(map(int,input().split()))
+    except:
+        break
+
+    gap1 = kang[1]-kang[0]
+    gap2 = kang[2]-kang[1]
+
+    if gap1>gap2:
+        print(gap1-1)
+    else:
+        print(gap2-1)
