156dd8b877aaac1fffec1378ec2b5795922b0200
heoseungyeon <dia0312@naver.com>
2021-02-21 15:48:03 +0900

    1260 fix, 2980 fix

--- syheo/codingTest1/Baekjoon/2980.py
+++ syheo/codingTest1/Baekjoon/2980.py
@@ -12,9 +12,9 @@ N, L = map(int,input().split())
 sec = 0 
 sangen = 0
 # 시간 총 합
-sum = 0
 info = deque([[] for _ in range(N)])
 
+#신호등 위치, 빨간불 시간, 초록불 시간 
 for i in range(N):
     D,R,G = map(int,input().split())
     info[i].append(D)
@@ -22,20 +22,34 @@ for i in range(N):
     info[i].append(G)
 
 while True:
+    #신호등이 남아 있을 경우 
     if len(info)!=0:
+        #신호등 위치에 상근이가 있을 경우 
         if info[0][0] == sangen:
+            #빨간불+초록불
             length = info[0][1]+info[0][2]
+            #빨간불인 경우 
             if (sec%length)<info[0][1]:
-                pass
+                sec+=(info[0][1]-(sec%length))
+            #초록불인 경우 
             else:
+                #이동 
                 info.popleft()
                 sangen+=1
+                sec+=1
+
         else:
-            sangen+=1
-        sec+=1
+            if info[0][0] < L:
+                sec+= info[0][0]-sangen
+                sangen= info[0][0]
+            else: 
+                sec += (L-sangen)
+        
     else:
+        #도착지점까지 점프 
         sec+=(L-sangen)
         break
+    #도착지점 도착시 
     if sangen==L:
         break
 
