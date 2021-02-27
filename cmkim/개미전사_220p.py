2a740e0c54b8001d433dbce6d1d4bfd378f1e838
bsyzch <bsyzch@gmail.com>
2021-02-27 14:37:39 +0900

    책DP숙제

---
+++ cmkim/개미전사_220p.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+dp = [0] * 101
+
+dp[1] = arr[1]
+dp[2] = max(arr[1], arr[2])
+for i in range(3, n+1):
+    dp[i] = max(dp[i - 1], dp[i - 2] + arr[i])
+
+print(dp[n])
