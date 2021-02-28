748cc2723ebddcd82ab76269d6417f6c0d1dd1c6
heoseungyeon <dia0312@naver.com>
2021-02-28 13:40:52 +0900

    예제 스터디 소스

--- syheo/codingTest1/Baekjoon/1003.py
+++ syheo/codingTest1/Baekjoon/1003.py
@@ -1,14 +1,37 @@
 #1003
 #피보나치 함수  
 
-def fibo(num,result):
-    if num ==0:
-        result[0]+=1
-        return 0
-    elif num == 1:
-        result[1]+=1
+#dp 배열 초기화 
+dzero= [0]*41
+done = [0]*41
+dp0 = [1,0]
+dp1 = [0,1]
+result = [0]*2
+
+def fibozero(num):
+    if num == 0: 
         return 1
-    else: return fibo(num-1,result)+fibo(num-2,result)
+    elif num == 1:
+        return 0
+    if dzero[num] != 0:
+        return dzero[num]
+    dzero[num] = fibozero(num-1) +fibozero(num-2)
+    return dzero[num]
+
+def fiboone(num):
+    if num == 1: 
+        return 1
+    elif num == 0:
+        return 0
+    if done[num] != 0:
+        return done[num]
+    done[num] = fiboone(num-1) +fiboone(num-2)
+    return done[num]
+
+#미리 계산 
+for i in range(2,41):
+    dp0.append(dp0[i-1]+dp0[i-2])
+    dp1.append(dp1[i-1]+dp1[i-2])
 
 #테스트 케이스
 T = int(input())
@@ -17,8 +40,7 @@ numList = []
 for i in range(T):
     numList.append(int(input()))
 
-for i in numList:
-    result = [0,0]
-    
-    fibo(i,result)
-    print(result[0],result[1],sep=" ")
+for i in numList:    
+    print(dp0[i],dp1[i],sep=" ")
+    #print(fibozero(i),fiboone(i),sep=" ")
+
