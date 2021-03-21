ad296f107ef8e6e1381e26aea3ce7a4cf3b361b6
bsyzch <47052601+bsyzch@users.noreply.github.com>
2021-03-21 23:47:34 +0900

    찬민 브랜치 정리

--- cmkim/BaekJoon/백준_1535.py
+++ cmkim/BaekJoon/백준_1535.py
@@ -6,6 +6,10 @@ J = list(map(int, input().split())) #기쁨
 L, J = [0] + L, [0] + J     #1번 인덱스부터 사용하기 위해서 맨 앞에 땡겨준다
 dp = [[0 for _ in range(101)] for _ in range(21)]
 
+for i in range(1, n+1):
+    for j in range(1, 101):
+        if L[i] <= j:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-L[i]] + J[i])
+        else: dp[i][j] = dp[i-1][j]
 
-#print(L)
-#print(J)
+print(dp[n][99])
