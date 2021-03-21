7982102e4431fa58127be83b5c1abdb52d06deb2
bsyzch <47052601+bsyzch@users.noreply.github.com>
2021-03-21 23:53:56 +0900

    찬민 백준정리 (#47)
    * 책숙제
    
    * 찬민 백준 숙제
    
    * 찬민 백준 숙제
    
    * 찬민 3/18 백준숙제
    
    * 찬민 브랜치 정리

--- cmkim/BaekJoon/백준_1535.py
+++ cmkim/BaekJoon/백준_1535.py
@@ -7,5 +7,10 @@ L, J = [0] + L, [0] + J     #1번 인덱스부터 사용하기 위해서 맨 앞
 dp = [[0 for _ in range(101)] for _ in range(21)]
 
 
-#print(L)
-#print(J)
+for i in range(1, n+1):
+    for j in range(1, 101):
+        if L[i] <= j:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-L[i]] + J[i])
+        else: dp[i][j] = dp[i-1][j]
+
+print(dp[n][99])
