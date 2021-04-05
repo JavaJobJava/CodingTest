ad1d922dd8982b801bf63e38e75107441ffdecd5
heoseungyeon <dia0312@naver.com>
2021-04-05 14:31:20 +0900

    6603

---
+++ syheo/codingTest1/Baekjoon/6603.py
@@ -0,0 +1,17 @@
+#solved.ac
+#실버2
+#
+#로또
+#6603
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+while True: 
+    sList = list(map(int,input().split()))
+    if sList[0]==0:
+        break
+    for item in list(combinations(sList[1:],6)):
+        print(*item)
+    print()
+
