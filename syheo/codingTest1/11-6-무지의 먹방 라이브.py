95564ee57d255f9ec5575987b1ab8afcf63787c8
heoseungyeon <dia0312@naver.com>
2021-04-13 16:49:57 +0900

    11-6 fix

--- syheo/codingTest1/11-6-무지의 먹방 라이브.py
+++ syheo/codingTest1/11-6-무지의 먹방 라이브.py
@@ -1,34 +1,41 @@
 #CH11 그리디 기출
 #예제 11-6
 #무지의 먹방 라이브
+import heapq
 
+# food_times = [3,1,2]
+# k=5
+# food_times = [3, 2, 2, 1, 1, 2, 4, 5]
+# k= 12
+food_times = [2, 2, 2, 1, 1, 2, 2, 2]
+k= 14
 def solution(food_times, k):
     answer = 0
+    rest = k
+    length = len(food_times)
     rotation = 1
-    curIdx= 0 
-    size = len(food_times)
-    #계수 정렬 
-    idxSorted = [0 for i in range(100000001)]
-    for i in range(size):
-        idxSorted[food_times[i]]+=1
-    #퀵 정렬
-    #food_times.sort()
+    q = []
+    for i in range(length):
+        heapq.heappush(q,(food_times[i],i))
+
     while True:
-        if size<=0:
-            answer = -1
-            break
-        elif k//size==0:
-            answer = k+1
-            break
-        else:
-            k-=size
-            #퀵정렬시
-            # while food_times[curIdx]==rotation:
-            #     size-=1
-            #     curIdx+=1
-            size-=idxSorted[rotation]
+        if  q and rest//len(q):  
+            rest-=1*len(q)
+            while q and q[0][0]==rotation:
+                heapq.heappop(q)
             rotation+=1
+        else:
+            q.sort(key=lambda x : x[1])
             
-                
+            if len(q)==0:
+                answer = -1
+            elif rest>=len(q):
+                answer = q[rest-len(q)][1]
+            else:
+                answer = q[rest][1]+1
+            break
 
     return answer
+
+
+print(solution(food_times,k))
