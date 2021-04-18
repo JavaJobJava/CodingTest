48e6966f6669373a41a31c2aeb8b5202b1da7344
Myunghwan Kim <samkim2626@gmail.com>
2021-04-19 01:41:06 +0900

    명환 숙제(04132021) (#61)
    * 명환 백준 숙제
    
    * 명환 책 숙제
    
    * 2667 수정

---
+++ mhkim/greedy-6.cpp
@@ -0,0 +1,77 @@
+/**
+ * @file greedy-6.cpp
+ * @brief 무지의 먹방 라이브
+ * @author Sam Kim (samkim2626@gmail.com)
+ * 
+ * 1. 처음에 tni 음식 시간, 음식 인덱스 pair로 만들어서 정렬함 -> 우선순위큐로
+ * 2. 우선순위큐 top이 시간이 가장 적게 걸리는 음식 -> 계산해서 k에서 빼기, 우선순위큐에서 빼기
+ * 3. 다음 반복에서 계산했을 때 k보다 크면 빼지 않고 다음에 먹어야 할 음식 인덱스 출력
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> food_times, long long k)
+{
+    int answer = 0;
+
+    long long sum = accumulate(food_times.begin(), food_times.end(), 0LL);
+    if (sum < k)
+    {
+        return -1;
+    }
+
+    // 정렬 전 time과 index를 저장
+    priority_queue<pair<int, int>> tni;
+    for (int i = 0; i < food_times.size(); i++)
+    {
+        tni.push(make_pair(-food_times[i], i + 1)); // 파이썬은 +
+    }
+
+    int nFoods = food_times.size(); // 남은 음식의 개수
+    int sumTime = 0;                // 먹는데 걸린 총 시간
+    while (1)
+    {
+        pair<int, int> top = tni.top();
+
+        if (sumTime + (-top.first) * nFoods > k)
+        {
+            answer = (k - sumTime) % nFoods break;
+        }
+
+        sumTime += (-top.first) * nFoods;
+        tni.pop();
+        nFoods--;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<int> food_times;
+    for (int i = 0; i < n; i++)
+    {
+        int time;
+        cin >> time;
+        food_times.push_back(time);
+    }
+
+    long long k;
+    cin >> k;
+
+    cout << solution(food_times, k) << '\n';
+
+    return 0;
+}
