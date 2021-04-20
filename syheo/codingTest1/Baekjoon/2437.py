5d57c492c78005b0ceb31e09102bc8764656f18e
heoseungyeon <dia0312@naver.com>
2021-04-20 10:50:26 +0900

    주석 담

--- syheo/codingTest1/Baekjoon/2437.py
+++ syheo/codingTest1/Baekjoon/2437.py
@@ -6,6 +6,11 @@
 import sys 
 input = sys.stdin.readline
 
+#아이디어 
+#ans ==> 현재까지 측정 가능한 물건의 최대 무게 
+#ans + 1 ==> 현재로 측정할 수 없는 최소 무게 
+#ans + chu ==> 만약 ans가 chu보다 크거나 같으면 ans 를 현재 추의 무게까지 더하여 갱신.
+
 N = int(input())
 ans = 1
 chuList = list(map(int,input().split()))
