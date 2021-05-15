c767b3fa328c716c4125b934ef812649f9fa71df
bsyzch <bsyzch@gmail.com>
2021-05-15 21:14:45 +0900

    변경사항

--- cmkim/2021 카카오 인턴쉽/프로그래밍1.py
+++ cmkim/2021 카카오 인턴쉽/프로그래밍1.py
@@ -6,31 +6,23 @@ def solution(s):
     arr = []
     i = 0
     while i < len(s):
-        #print('i = ', i)
 
         if s[i].isdigit():
             arr.append(int(s[i]))
 
         else:
             j = 0
-            #print('i = ', i)
-            # print(s[i:])
             while j < 10:
                 if num[j] in s[i:i+5]:
-                    #print('num[j] =', num[j])
                     arr.append(j)
                     i += len(num[j]) - 1
                     break
                 j += 1
         i += 1
 
-    #print(arr)
     answer = int("".join(map(str, arr)))
-    #print('+'.join(arr))
-    #answer = int("".join(arr))
     print(answer)
     return answer
 
-#solution("one4seveneight")
-solution('1zerotwozero3')
+#solution('1zerotwozero3')
 
