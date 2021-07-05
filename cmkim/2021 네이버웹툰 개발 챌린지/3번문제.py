f857d9e3d82f44645c72cbefcbaf3d3fd2821007
bsyzch <bsyzch@gmail.com>
2021-07-05 10:34:20 +0900

    네이버웹툰 개발코테

---
+++ cmkim/2021 네이버웹툰 개발 챌린지/3번문제.py
@@ -0,0 +1,18 @@
+from _collections import deque
+import copy
+
+def dfs(a, k):
+    q = deque()
+
+    for i in range(len(a)):
+        if a[i] =
+
+
+    return
+
+def solution(arr, k):
+    answer = -1
+    a = copy.deepcopy(arr)
+    dfs(a, k)
+
+    return answer
